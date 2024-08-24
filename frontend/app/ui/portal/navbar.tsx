b24fbdd118c8567d42e3e1d3ea6850888780a66a
Qingquan Li <liqqisme@gmail.com>
2024-08-23 23:52:26 -0400

    Update buttons color to use bc-red and bc-yellow (#32)

--- frontend/app/ui/portal/navbar.tsx
+++ frontend/app/ui/portal/navbar.tsx
@@ -42,7 +42,11 @@ export default function NavBar() {
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
           {navigation.map((item) => (
-            <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-sm font-semibold leading-6 text-gray-900 hover:text-bc-red"
+            >
               {item.name}
             </a>
           ))}
@@ -83,7 +87,7 @@ export default function NavBar() {
                   <a
                     key={item.name}
                     href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-bc-yellow hover:text-bc-red"
                   >
                     {item.name}
                   </a>
