604271294e0c6e615247da605152d69da83afa22
Qingquan Li <liqqisme@gmail.com>
2024-09-06 23:19:26 -0400

    Build the events page and add Club Hours section (#55)

--- frontend/app/(portal)/events/page.tsx
+++ frontend/app/(portal)/events/page.tsx
@@ -1,10 +1,11 @@
+import HeroSection from "@/app/ui/portal/events/HeroSection";
+import ClubHours from "@/app/ui/portal/events/ClubHours";
+
 export default function Page() {
   return (
-    <div className="flex justify-center items-center h-screen bg-gradient-to-b from-bc-red/15">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold">Events Page</h1>
-        <p>development in progress...</p>
-      </div>
-    </div>
+    <main>
+      <HeroSection />
+      <ClubHours />
+    </main>
   );
 }
---
+++ frontend/app/ui/portal/events/HeroSection.tsx
@@ -0,0 +1,16 @@
+export default function HeroSection() {
+  return (
+    <div className="relative isolate px-6 pt-14 lg:px-8 bg-gradient-to-b from-bc-red/15">
+      <div className="mx-auto max-w-2xl py-20 sm:py-24 lg:py-28">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            Events
+          </h1>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            At the Computer Science Club, we are committed to fostering a dynamic and engaging community through a series of events and workshops designed to enhance your learning experience and professional growth.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
