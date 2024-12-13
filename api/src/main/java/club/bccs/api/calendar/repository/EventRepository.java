51b5c81c4a8170253b243aa00c5c00106807758f
Qingquan Li <liqqisme@gmail.com>
2024-12-13 18:02:49 -0500

    Add slug, update api endpoints with correct status codes

--- api/src/main/java/club/bccs/api/calendar/repository/EventRepository.java
+++ api/src/main/java/club/bccs/api/calendar/repository/EventRepository.java
@@ -1,5 +1,7 @@
 package club.bccs.api.calendar.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,7 @@ import club.bccs.api.calendar.entity.Event;
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
     // Add custom query methods if needed
+
+    // Find event by slug
+    Optional<Event> findBySlug(String slug);
 }
