221a577e42425e9d03a5198d14774c30297f66ea
birongliu <birongliu2003@gmail.com>
2024-12-14 23:04:01 -0500

    Refactor event fetching logic to filter active events and remove unused code

--- frontend/app/(portal)/events/page.tsx
+++ frontend/app/(portal)/events/page.tsx
@@ -19,13 +19,11 @@ export default function Page() {
     // Fetch events from the backend
     const event = async () => {
       const event = await getEventAction();
-      console.log("event", event);
       if(!event) {
         setEvents([])
+        return;
       }
-      setEvents(event);
-      console.log("events", event);
-      setEvents(event)
+      setEvents(event.filter(k => k.isActive));
     };
     event();
   }, []);
@@ -43,62 +41,7 @@ export default function Page() {
   return (
     <main>
       <HeroSection />
-      {events.length > 0 && (
-        <CalendarSection handleEventClick={handleEventClick} events={events} />
-      )}
+      <CalendarSection handleEventClick={handleEventClick} events={events} />
     </main>
   );
 }
-
-/**
- * 
-    // setEvents([
-    //   {
-    //     title: 'Club Hours',
-    //     description: 'Weekly club meeting to discuss ongoing projects, coding, and collaboration.',
-    //     location: 'Room 0317, Ingersoll Hall',
-    //     rrule: {
-    //       freq: 'weekly', // Recurring every week
-    //       interval: 1,    // Every week
-    //       byweekday: 'TU', // Tuesday
-    //       dtstart: '2024-09-03T13:00:00', // Start time in UTC (01:00 PM local time)
-    //       until: '2024-10-31', // Until the end of the year
-    //     },
-    //     duration: '02:30', // Duration (2 hours and 30 minutes)
-    //     exdate: [
-    //       '2024-10-08T13:00:00', // Exclude October 8, 2024
-    //     ],
-    //   },
-    //   {
-    //     title: 'Club Hours',
-    //     description: 'Weekly club meeting to discuss ongoing projects, coding, and collaboration.',
-    //     location: 'Room 0317, Ingersoll Hall',
-    //     rrule: {
-    //       freq: 'weekly', // Recurring every week
-    //       interval: 1,    // Every week
-    //       byweekday: 'WE', // Wednesday
-    //       dtstart: '2024-09-04T12:00:00', // Start time in UTC (12:00 PM local time)
-    //       until: '2024-10-31', // Until the end of the year
-    //     },
-    //     duration: '02:00', // Duration (2 hours)
-    //     exdate: [
-    //       '2024-10-02T12:00:00', // Exclude October 2, 2024
-    //     ],
-    //   },
-    //   {
-    //     title: 'Club Connection & Career Panel',
-    //     description: 'Get to know the executive board members and hear from 4 panelists about their experiences in the tech industry.',
-    //     location: 'Jefferson Williams 4th Fl, Student Center',
-    //     start: '2024-10-08T01:00:00', 
-    //     end: '2024-10-08T14:30:00',
-    //   },
-    //   {
-    //     title: 'Virtual Career Panel',
-    //     description: 'Get to know and hear from 5 BC alumni and previous e-board members about their experiences in the tech industry.',
-    //     location: 'Virtual, RVSP for Zoom link: https://qr-code.click/i/670f06f119cc7',
-    //     start: '2024-10-19T05:00:00', 
-    //     end: '2024-10-19T06:00:00',
-    //   },
-    // ]);
- * 
- */
