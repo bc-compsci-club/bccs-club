70e3ce14b154b7f36a8f49e910e5f98e71becb5e
birongliu <birongliu2003@gmail.com>
2024-12-23 21:40:17 -0500

    initalize game jam

---
+++ frontend/app/ui/portal/game-jam-2025/Hero.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='elative isolate px-6 pt-14 lg:px-8 bg-gradient-to-b from-bc-red/15'>
+        <div
+        aria-hidden="true"
+        className="absolute inset-x-0 -top-16 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl pointer-events-none"
+      >
+        <div
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+          className="aspect-[1318/752] w-[82.375rem] flex-none bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-15"
+        />
+      </div>
+      a
+    </div>
+  )
+}
