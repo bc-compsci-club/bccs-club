5949d446666d448617341c9c69ee4cb20a68be25
birongliu <birongliu2003@gmail.com>
2024-12-25 11:11:29 -0500

    Add FAQ component and link to registration in Hero for Game Jam 2025

---
+++ frontend/app/ui/portal/game-jam-2025/FAQ.tsx
@@ -0,0 +1,66 @@
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react";
+import { BsPlus } from "react-icons/bs";
+import { BsDash } from "react-icons/bs";
+
+const items = [
+  {
+    title: "What is Game Jam 2025?",
+    message:
+      "The Game Jam 2025 is a 1 week long, beginner-friendly competition between developers to create a game within a short amount of time.",
+  },
+  {
+    title: "Who can participate in Game Jam 2025?",
+    message:
+      "The Game Jam 2025 is open to all CSI and Brooklyn College students and majors.",
+  },
+  {
+    title: "Is there a discord for Game Jam 2025?",
+    message: "Yes, there is a discord. You can join the discord here: https://discord.com/invite/q4Yyx3dhJC.",
+  },
+  {
+    title: "What is the team limit for Game Jam 2025? Can I participate alone?",
+    message: "The team limit for Game Jam 2025 is 4. You can participate alone or with a team.",
+  },
+];
+
+export default function FAQ() {
+  return (
+    <div className="mx-auto max-w-7xl px-6 lg:px-8 py-8">
+      <div className="mx-auto max-w-4xl divide-y divide-gray-900/10">
+        <h2 className="text-2xl font-bold leading-10 tracking-tight text-gray-900">
+          Frequent Asked Questions
+        </h2>
+        <dl className="mt-2 space-y-6 divide-y divide-gray-900/10">
+          {items.map((item) => (
+            <Disclosure key={item.title} as="div" className="pt-6">
+              <dt>
+                <DisclosureButton className="group flex w-full items-start justify-between text-left text-gray-900">
+                  <span className="text-base font-semibold leading-7">
+                    {item.title}
+                  </span>
+                  <span className="ml-6 flex h-7 items-center">
+                    <BsPlus
+                      aria-hidden="true"
+                      className="h-6 w-6 group-data-[open]:hidden"
+                    />
+                    <BsDash
+                      aria-hidden="true"
+                      className="h-6 w-6 [.group:not([data-open])_&]:hidden"
+                    />
+                  </span>
+                </DisclosureButton>
+              </dt>
+              <DisclosurePanel as="dd" className="mt-2 pr-12">
+                <p className="text-lg  text-gray-600">{item.message}</p>
+              </DisclosurePanel>
+            </Disclosure>
+          ))}
+        </dl>
+      </div>
+    </div>
+  );
+}
