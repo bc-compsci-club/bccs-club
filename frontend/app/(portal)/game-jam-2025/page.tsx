7e126f11a22111f219e197c8723ab22f50eb9d91
birongliu <birongliu2003@gmail.com>
2024-12-28 00:15:47 -0500

    added Criteria

--- frontend/app/(portal)/game-jam-2025/page.tsx
+++ frontend/app/(portal)/game-jam-2025/page.tsx
@@ -1,6 +1,7 @@
 import Contact from '@/app/ui/portal/game-jam-2025/Contact'
 import FAQ from '@/app/ui/portal/game-jam-2025/FAQ'
 import Hero from '@/app/ui/portal/game-jam-2025/Hero'
+import JudgeCriteria from '@/app/ui/portal/game-jam-2025/JudgeCriteria'
 import Judges from '@/app/ui/portal/game-jam-2025/Judges'
 import Schedule from '@/app/ui/portal/game-jam-2025/Schedule'
 import React from 'react'
@@ -11,6 +12,7 @@ export default function page() {
     <Hero />
     <Schedule />
     <Judges />
+    <JudgeCriteria />
     <FAQ />
     <Contact />
    </main>
