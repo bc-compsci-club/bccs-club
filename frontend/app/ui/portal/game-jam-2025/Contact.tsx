eff495cb78015323cb485ef45737801de9fc8fcc
birongliu <birongliu2003@gmail.com>
2024-12-27 23:45:45 -0500

    Update Contact component layout for improved text alignment

--- frontend/app/ui/portal/game-jam-2025/Contact.tsx
+++ frontend/app/ui/portal/game-jam-2025/Contact.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <div className="max-w-7xl py-8 justify-center flex flex-col items-center">
+    <div className="max-w-7xl py-8 mx-auto text-center">
       <p className="text-sm leading-8 text-gray-600">
         If you have any questions or concerns, please reach out to us at
       </p>
